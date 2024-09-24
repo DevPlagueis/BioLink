1a2b4fe135f54cb0bd48c9cf3aa6cab7a79f41d8
Mike Cuesta <Cuestam.dev@gmail.com>
2024-09-24 13:56:34 -0400

    Signup form now submits and takes / uses data currently in it

--- 9.24.24-TestApp/bio_link/src/SignUp/SignUp.js
+++ 9.24.24-TestApp/bio_link/src/SignUp/SignUp.js
@@ -1,20 +1,74 @@
 import "./SignUp.css"
 import SignUpBtn from "./SignUpBtn";
+import React, { useState } from 'react';
+
 function SignUp() {
-    return(
-        <>
-            <div id="SignUpContainer">
-                <h1>SignUp</h1>
-                <form id = "SignUpForm">
-                        <input name ="fname" type="text" placeholder="First Name"/>
-                        <input name="lname" type="text" placeholder="Last Name"/>
-                        <input name="email" type="text" placeholder="Email-Address"/>
-                        <input name="password" type="password" placeholder="Password"/>
-                </form>
-                <SignUpBtn />
-             </div>
-        </>
-    )
+  // Step 1: Create state variables for each form field
+  const [fname, setfName] = useState('');
+  const [lname, setlName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission
+    console.log('fName:', fname);
+    console.log('lName:', lname);
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    //  Do something with the values 
+    let str = "cuestam.dev@gmail.com";
+    // Find the index of the '@' symbol
+    let atSymb = str.indexOf('@');
+
+    // Strip out the part starting at '@'
+    let result = str.substring(atSymb + 1); // "gmail.com"
+    console.log("Email Domain: " + result);
+  };
+
+  return (
+    <>
+        <div id="SignUpContainer">
+        <h1>Sign Up</h1>
+            <form id="SignUpForm" onSubmit={handleSubmit}>
+                <div>
+                    <input 
+                    type="text" 
+                    placeholder="First Name"
+                    value={fname} 
+                    onChange={(e) => setfName(e.target.value)} // Update state on change
+                    />
+                </div>
+                <div>
+                    <input 
+                    type="text" 
+                    placeholder="Last Name"
+                    value={lname} 
+                    onChange={(e) => setlName(e.target.value)} // Update state on change
+                    />
+                </div>
+                <div>
+                    <input 
+                    type="email" 
+                    placeholder="Email-Address"
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} // Update state on change
+                    />
+                </div>
+                <div>
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} // Update state on change
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </>
+  );
 }
 
 export default SignUp;
+
--- 9.24.24-TestApp/bio_link/src/SignUp/SignUpBtn.js
+++ 9.24.24-TestApp/bio_link/src/SignUp/SignUpBtn.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import "./SignUp.css"
+import "./SignUp.js"
 
 function SignUpBtn() {
+    const signedUp = () => {
+        console.log("Someone clicked Sign Up")
+        console.log()
+    }
+    
     return (
         <>
-            <button id="SignUpBtn">
+            <button id="SignUpBtn" onClick={signedUp}>
                 Sign Up
             </button>
+
         </>
     )
 }
