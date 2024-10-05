35d1786e6510c58a1c578be568c6e9da426320fc
Mike Cuesta <Cuestam.dev@gmail.com>
2024-10-05 08:52:32 -0400

    working on dynamicly making cards re-purposing code from personal project to grab data from data matrix's data should then run through a function to allocate unique data elements to single cards then print into cards area

--- bio-skeleton/app.js
+++ bio-skeleton/app.js
@@ -1,20 +1,54 @@
-function createAllCards(dataMatrix) {
-    for (const { id, title, img, desc} of dataMatrix) {
-        // Pass unique text content, ID, and onclick handler for each card
-        const clickHandler = () => //
-        Swal.fire({
-            imageUrl: img,
-            text: desc,
-            showClass: {
-                popup: 'animate__animated animate__fadeInDown'
-            },
-            hideClass: {
-                popup: 'animate__animated animate__fadeOutUp'
-            }
-        });
-        makeCard(title, id, clickHandler);
-    }
-}
+// function createAllCards(dataMatrix) {
+//     for (const { id, title, img, desc} of dataMatrix) {
+//         // Pass unique text content, ID, and onclick handler for each card
+//         const clickHandler = () => //
+//         Swal.fire({
+//             imageUrl: img,
+//             text: desc,
+//             showClass: {
+//                 popup: 'animate__animated animate__fadeInDown'
+//             },
+//             hideClass: {
+//                 popup: 'animate__animated animate__fadeOutUp'
+//             }
+//         });
+//         makeCard(title, id, clickHandler);
+//     }
+// }
+
+// function makeCard(textContent, cardId, clickHandler) {
+//     const card = document.createElement('button');
+//     const page = document.querySelector('#pageContent');
+//     card.textContent = textContent;
+//     card.id = cardId;
+//     // Set onclick handler based on the provided function
+//     card.onclick = clickHandler;
+//     card.classList.add('Card');
+
+//     //clickFlag handler
+//     card.clickFlag  = false
+//     card.addEventListener('click', function cardClicked(cardId) {
+//         if (!this.clickFlag) {//if false
+//             console.log(`Current clickFlag for ${this.d} before click: ${this.clickFlag}`);
+//             this.clickFlag = true; // Set clickFlag to true
+//             console.log(`cickFlag after click: ${this.clickFlag}`)
+//             console.log(`Number of total clicks so far: ${numClicks}`);
+//             numClicks++; // Increment the number of total clicks
+//             console.log(`NUMBER OF CLICKS NOW THAT ${this.id} was clicked: ${numClicks}`);
+//         } else {
+//             if (numClicks === 4) {
+//                 const component = document.querySelector('.hidden');
+//                 component.style.visibility = 'visible';
+//             }
+//             console.log(`Card ${this.id} already clicked.`);
+//         }
+
+//     });
+//     //function to...
+//     page.appendChild(card);
+// }
+
+//////////////////////////////
 
 function makeCard(textContent, cardId, clickHandler) {
     const card = document.createElement('button');
@@ -24,39 +58,7 @@ function makeCard(textContent, cardId, clickHandler) {
     // Set onclick handler based on the provided function
     card.onclick = clickHandler;
     card.classList.add('Card');
-
-    //clickFlag handler
-    card.clickFlag  = false
-    card.addEventListener('click', function cardClicked(cardId) {
-        if (!this.clickFlag) {//if false
-            console.log(`Current clickFlag for ${this.d} before click: ${this.clickFlag}`);
-            this.clickFlag = true; // Set clickFlag to true
-            console.log(`cickFlag after click: ${this.clickFlag}`)
-            console.log(`Number of total clicks so far: ${numClicks}`);
-            numClicks++; // Increment the number of total clicks
-            console.log(`NUMBER OF CLICKS NOW THAT ${this.id} was clicked: ${numClicks}`);
-        } else {
-            if (numClicks === 4) {
-                const component = document.querySelector('.hidden');
-                component.style.visibility = 'visible';
-            }
-            console.log(`Card ${this.id} already clicked.`);
-        }
-
-    });
-    //function to...
-    page.appendChild(card);
 }
+// cards_container.appendChild(card)
+// cards_container.appendChild(card)
 
-//////////////////////////////
-
-const cards_container = document.getElementById("feed-cards")
-const card = document.createElement('div')
-card.classList.add('cards')
-card.textContent = "card"
-
-// for (let i = 0; i < 5; i++) {
-//     cards_container.appendChild(card)
-// }
-cards_container.appendChild(card)
-cards_container.appendChild(card)
--- bio-skeleton/data.js
+++ bio-skeleton/data.js
@@ -45,3 +45,38 @@ CARD
     BODY
 CARD
 */
+
+///////////////
+//DATA for sample card//
+const contacts = [
+    {
+        contact_id: '00',
+        first_name: 'Michael',
+        last_name: 'Cuesta',
+        contact_affil: 'ABC UNIVERSITY',
+        email: 'mikesFake@email.com',
+
+    },
+    {
+        contact_id: '01',
+        first_name: 'Christian',
+        last_name: 'Vega',
+        contact_affil: 'XYZ UNIVERSITY',
+        email: 'vegaFake@email.com',
+
+    },
+    {
+        contact_id: '02',
+        first_name: 'Vic',
+        last_name: 'Lapost',
+        contact_affil: 'QRS UNIVERSITY',
+        email: 'vicFake@email.com',
+
+    }
+]
+const cardData = [
+    {
+        contact_name: ""
+    }
+]
+
