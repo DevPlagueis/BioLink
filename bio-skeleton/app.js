de919dd1c1aee2601ae36215bfa8a6e45f8fa1ea
Mike Cuesta <Cuestam.dev@gmail.com>
2024-10-05 10:54:28 -0400

    Working on placement / behavior of cards trying to get card to have a set height based on content and basic width hight should not go over x pixles / % of container and width should remain constant need to play around more with the  'make cards' function to get this down. solid spot to step away from project

--- bio-skeleton/app.js
+++ bio-skeleton/app.js
@@ -48,17 +48,85 @@
 //     page.appendChild(card);
 // }
 
-//////////////////////////////
+////////////////
+///FORMATTED DATE FUNCTION
 
-function makeCard(textContent, cardId, clickHandler) {
-    const card = document.createElement('button');
-    const page = document.querySelector('#pageContent');
-    card.textContent = textContent;
-    card.id = cardId;
-    // Set onclick handler based on the provided function
-    card.onclick = clickHandler;
-    card.classList.add('Card');
+function formatDate() {
+    //TODAYS DATE ONLY
+    const today = new Date();
+
+    const month = String(today.getMonth() + 1).padStart(2, '0')
+    const day = String(today.getDate()).padStart(2, '0')
+    const year = today.getFullYear();
+
+    const formattedDate = `${month}/${day}/${year}`
+    return formattedDate
 }
-// cards_container.appendChild(card)
-// cards_container.appendChild(card)
+///////////////
+//DATA for sample card//
+//below is matrix of users
+//find way to create this from flat / json file
+const contact_matrix = [
+    {
+        contact_id: '000',
+        first_name: 'Michael',
+        last_name: 'Cuesta',
+        contact_affil: 'ABC UNIVERSITY',
+        contact_email: 'mikesFake@email.com',
 
+    },
+    {
+        contact_id: '001',
+        first_name: 'Christian',
+        last_name: 'Vega',
+        contact_affil: 'XYZ UNIVERSITY',
+        contact_email: 'vegaFake@email.com',
+
+    },
+    {
+        contact_id: '002',
+        first_name: 'Vic',
+        last_name: 'Lapost',
+        contact_affil: 'QRS UNIVERSITY',
+        contact_email: 'vicFake@email.com',
+
+    }
+]
+
+function makeCards(dict) {
+
+    for (const { contact_id, first_name, last_name, contact_affil, contact_email} of dict) {
+        console.log(first_name + ' ' + last_name)
+        
+        const feed = document.getElementById('feed-cards')
+        const card = document.createElement('div')
+            card.classList.add('cards')
+        //sections of card
+        const header = document.createElement('div')
+        const header_left = document.createElement('div')
+            header_left.classList.add('header_left', 'header_section')
+        const header_right = document.createElement('div')
+            header_right.classList.add('header_right', 'header_section')
+        header.classList.add('card_header')
+            const user_full_name = document.createElement('div')
+                user_full_name.textContent = first_name + ' ' + last_name
+            const post_date = document.createElement('div')
+                post_date.textContent = formatDate()
+            const indicator = document.createElement('div')
+                indicator.textContent = 'false'
+            
+            header_left.appendChild(user_full_name)
+            header_right.appendChild(post_date)
+            header_right.appendChild(indicator)
+
+            header.appendChild(header_left)
+            header.appendChild(header_right)
+
+
+        card.appendChild(header)
+        feed.appendChild(card)
+
+    }
+}
+
+makeCards(contact_matrix)
